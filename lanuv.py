f9f78f49aabc355c86c5d0722dd30a40f063929b
Jörn Smock <jsmock@posteo.de>
2024-05-17 17:50:56 +0200

    resolve merge conflict (diffput didn't suffice?!)

--- lanuv.py
+++ lanuv.py
@@ -4,11 +4,7 @@
 # Web-Scraping, sort-of
 # ...my usual overkill: I use mighty tools to do
 # only a tiny little thing.
-<<<<<<< HEAD
-# (another comment just to get a merge conflict)
-=======
 # (beautifulsoup in this case)
->>>>>>> merge-test
 
 from argparse import ArgumentParser
 parser = ArgumentParser(description='fetch a temperature from a LANUV station')
